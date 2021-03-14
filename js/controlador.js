eeaf3b86d7d07ee145d23964b465e650e212d3e8
Rivera_Enoc <enocantrivmor@yahoo.com>
2021-03-13 20:00:44 -0600

    Barra-Navegación pagina de desarrollador TERMINADA

--- js/controlador.js
+++ js/controlador.js
@@ -3,4 +3,12 @@ HTML -><i class="fab fa-html5"></i>
 JS   -><i class="fab fa-js"></i>
 MUNDO-><i class="fas fa-globe"></i>
 CSS -><i class="fab fa-css3"></i>
+
+
+<i class="fas fa-folder-open"></i>
+
+<i class="fas fa-folder-minus"></i>
+<i class="fas fa-user-friends"></i>
+<i class="fas fa-user-plus"></i>
+<i class="fas fa-user-cog"></i>
 */
